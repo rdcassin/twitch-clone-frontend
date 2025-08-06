7aea8642636ca1554eaba32f3bd650d9b53471bc
rdcassin <rdcassin@gmail.com>
2025-08-06 15:59:26 -0500

    added createdAt ordering for getFollowedUsers

--- lib/services/follow-service.ts
+++ lib/services/follow-service.ts
@@ -27,13 +27,18 @@ export const getFollowedUsers = async () => {
           },
         },
       },
-      orderBy: {
-        following: {
-          stream: {
-            isLive: "desc",
+      orderBy: [
+        {
+          following: {
+            stream: {
+              isLive: "desc",
+            },
           },
         },
-      },
+        {
+          createdAt: "desc",
+        },
+      ],
     });
 
     return followedUsers;
